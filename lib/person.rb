3d101210f6f5ea5a75e0625f851aacae514b711f
Christopher Kennedy <chriswkennedy@icloud.com>
2019-08-28 14:09:28 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person 
+  
+  
+  def name #getter
+    @name
+  end
+  
+  def name=(person_name) #setter
+    @name = person_name
+  end
+  
+  def job #getter 
+    @job
+  end
+  
+  def job=(person_job) #setter
+    @job = person_job
+  end
+  
+  
+end 
