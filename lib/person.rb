b793c63e1c8cc163fe7a62b85e7ad1184ade619c
Sarah Drake <jussaradrake@outlook.com>
2019-08-28 14:05:41 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,33 @@
+class Person
+ 
+   def name=(person_name)
+    @name = person_name
+  end
+ 
+ 
+  def name
+    @name
+  end
+end
+ 
+    beyonce = Person.new
+    beyonce.name = "fido"
+     
+    puts beyonce.name
+
+class Person
+  
+def job= (person_job)
+  @job = (person_job)
+end
+
+
+def job
+   @job
+ end
+end
+
+    beyone = Person.new
+    beyonce.job = "beyonce"
+    
+    puts beyonce.job
